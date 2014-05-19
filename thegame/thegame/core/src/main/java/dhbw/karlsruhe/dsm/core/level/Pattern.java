73d747a4631f868f47e208ab82610492baaf99b4
hipsterfister <weissnicht05@web.de>
2014-05-19 09:20:15 +0200

    Update Pattern.java
    Pattern.java Prototype

--- thegame/thegame/core/src/main/java/dhbw/karlsruhe/dsm/core/level/Pattern.java
+++ thegame/thegame/core/src/main/java/dhbw/karlsruhe/dsm/core/level/Pattern.java
@@ -1,5 +1,55 @@
 package dhbw.karlsruhe.dsm.core.level;
 
-public class Pattern {
+import java.util.List;
 
+public class Pattern {
+	private int originX;
+	private int originY;
+	private int height;
+	private int width;
+	
+	private List<PatternPrototype> availablePrototypes;
+	
+	public Pattern(int originX, int originY) {
+		this.originX = originX;
+		this.originY = originY;
+		
+		//this.pP = new PatternPrototype();
+	}
+	
+	private void loadPrototypes() {
+		
+	}
+	
+	public int getOriginX() {
+		return originX;
+	}
+	
+	public void setOriginX(int originX) {
+		this.originX = originX;
+	}
+	
+	public int getOriginY() {
+		return originY;
+	}
+	
+	public void setOriginY(int originY) {
+		this.originY = originY;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public void setWidth(int width) {
+		this.width = width;
+	}
 }
