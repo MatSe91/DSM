4b7ca8d82030be9f4f125c75f00899d31f7591f3
hipsterfister <weissnicht05@web.de>
2014-05-22 11:02:23 +0200

    added patterns

--- thegame/thegame/core/src/main/java/dhbw/karlsruhe/dsm/core/level/Pattern.java
+++ thegame/thegame/core/src/main/java/dhbw/karlsruhe/dsm/core/level/Pattern.java
@@ -1,55 +1,17 @@
 package dhbw.karlsruhe.dsm.core.level;
 
-import java.util.List;
+import com.badlogic.gdx.graphics.g2d.PolygonRegion;
+import com.badlogic.gdx.graphics.g2d.PolygonSprite;
 
-public class Pattern {
-	private int originX;
-	private int originY;
-	private int height;
-	private int width;
-	
-	private List<PatternPrototype> availablePrototypes;
-	
-	public Pattern(int originX, int originY) {
-		this.originX = originX;
-		this.originY = originY;
-		
-		//this.pP = new PatternPrototype();
+
+public class Pattern extends PolygonSprite{
+
+	public Pattern(PolygonRegion region) {
+		super(region);
 	}
 	
-	private void loadPrototypes() {
-		
+	public Pattern(PolygonSprite sprite) {
+		super(sprite);
 	}
-	
-	public int getOriginX() {
-		return originX;
-	}
-	
-	public void setOriginX(int originX) {
-		this.originX = originX;
-	}
-	
-	public int getOriginY() {
-		return originY;
-	}
-	
-	public void setOriginY(int originY) {
-		this.originY = originY;
-	}
-	
-	public int getHeight() {
-		return height;
-	}
-	
-	public void setHeight(int height) {
-		this.height = height;
-	}
-	
-	public int getWidth() {
-		return width;
-	}
-	
-	public void setWidth(int width) {
-		this.width = width;
-	}
+
 }
