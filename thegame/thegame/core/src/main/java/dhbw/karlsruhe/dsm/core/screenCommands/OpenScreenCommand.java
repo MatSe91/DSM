5081ab28ca0157c29b97aeb812f67b96eaa5ca98
hipsterfister <weissnicht05@web.de>
2014-06-11 12:23:56 +0200

    openscreencommand extends screenchangecommand

--- thegame/thegame/core/src/main/java/dhbw/karlsruhe/dsm/core/screenCommands/OpenScreenCommand.java
+++ thegame/thegame/core/src/main/java/dhbw/karlsruhe/dsm/core/screenCommands/OpenScreenCommand.java
@@ -1,19 +1,11 @@
 package dhbw.karlsruhe.dsm.core.screenCommands;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 
-import dhbw.karlsruhe.dsm.core.DSM;
-
-public abstract class OpenScreenCommand {
-	protected final DSM game = (DSM) Gdx.app.getApplicationListener();
-	
-	public OpenScreenCommand() {
-	}
-	
-	public abstract void execute();
+public abstract class OpenScreenCommand extends ScreenChangeCommand {
 	
 	protected void baseExecute(Screen next) {
+		game.getScreen().hide();
 		game.setScreen(next);
 	}
 }
